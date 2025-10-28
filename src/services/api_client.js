c3cfbdc88441cb0c271504be1107cfb4acee36ea
Neal Ormsbee <neal.ormsbee@gmail.com>
2025-10-28 09:51:13 -0600

    Fix a bug introduced when creating the centralized API client (#120)
    Signed-off-by: Neal Ormsbee <neal.ormsbee@ibm.com>

--- src/services/api_client.js
+++ src/services/api_client.js
@@ -1,11 +1,14 @@
 /**
  * Provide an axios client defining any configurations consistently used when communicating with an opencost server.
- * 
+ *
  * We should use this client when performing XHR requests across the application, rather than using `fetch` or `axios` directly.
  */
 import axios from "axios";
 
-const baseURL = process.env.BASE_URL || "http://localhost:9090/model";
+let baseURL = process.env.BASE_URL || "{PLACEHOLDER_BASE_URL}";
+if (baseURL.includes("PLACEHOLDER_BASE_URL")) {
+  baseURL = "http://localhost:9090";
+}
 
 const client = axios.create({ baseURL });
 

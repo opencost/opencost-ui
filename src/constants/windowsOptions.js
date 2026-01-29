d7386ac5395e1f18b1bd48f6ced3d90e9c469ee7
Ramez Medhat <iramezdev@gmail.com>
2026-01-29 23:02:19 +0200

    Move window options to a separate constants file for better organization
    Signed-off-by: Ramez Medhat <iramezdev@gmail.com>

---
+++ src/constants/windowsOptions.js
@@ -0,0 +1,12 @@
+const windowOptions = [
+  { name: "Today", value: "today" },
+  { name: "Yesterday", value: "yesterday" },
+  { name: "Last 24h", value: "24h" },
+  { name: "Last 48h", value: "48h" },
+  { name: "Week-to-date", value: "week" },
+  { name: "Last week", value: "lastweek" },
+  { name: "Last 7 days", value: "7d" },
+  { name: "Last 14 days", value: "14d" },
+];
+
+export { windowOptions };
